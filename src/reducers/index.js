7c172982e400450bd2f3ae5d5267d0f9e538d8d1
Gabriel <rotgergabriel@gmail.com>
2021-01-03 18:40:18 -0300

    feat-saved for energy problem

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { bindActionCreators } from "redux";
+import { setFavorite, deleteFavorite } from "../actions";
+
+const reducer = (state, action) => {
+    
+    switch(action.type) {
+        case 'SET_FAVORITE':
+            return {
+                ...state,
+                myList: [...state.myList, action.payload]
+            }
+        case 'DELETE_FAVORITE':
+            return{
+                ...state,
+                myList: state.myList.filter(items => items.id !== action.payload)
+            }
+        default:
+            return state;
+    }
+
+}
+
+export default reducer;
